3be8408979a8c5a3e2dded03c6a70abbaac6c4a0
Amarpreet Singh <amarpreet2209@gmail.com>
2017-09-26 17:27:00 +0530

    11

--- src/app/index.js
+++ src/app/index.js
@@ -5,8 +5,11 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
-  render() {
+  onGreet() {
+      alert("Hello!");
+  }
 
+  render() {
     return (
       <div className="container">
         <div className="row">
@@ -16,7 +19,7 @@ class App extends React.Component {
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home name={"Max"} initialAge={27} />
+            <Home name={"Max"} initialAge={27} greet={this.onGreet}/>
           </div>
         </div>
       </div>
